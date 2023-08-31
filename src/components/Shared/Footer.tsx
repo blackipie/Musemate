2a2141dde31ffacc5727e8b03c552e530f0e46ef
Shafin <blackipie@outlook.com>
2023-08-31 18:45:37 +0530

    init

--- src/components/Shared/Footer.tsx
+++ src/components/Shared/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
       
       <footer className='p-2 pt-4 lg:pb-0 lg:bottom-0'>
          <div className='flex text-xs gap-x-1 text-neutral-600'>
-         Copyright <Copyright size={16}/> {new Date().getFullYear()}  Devsects  
+         Copyright <Copyright size={16}/> {new Date().getFullYear()} Musemate  
         </div>
      </footer>
       
