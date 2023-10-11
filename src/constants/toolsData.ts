140e93df6e6c5f3c7be41d587ce6e5715e7f158f
Shafin <82350431+msaoct@users.noreply.github.com>
2023-10-11 22:25:12 +0530

    Update toolsData.ts

--- src/constants/toolsData.ts
+++ src/constants/toolsData.ts
@@ -8,21 +8,21 @@ export interface CardData {
 }
 
 export const SecuritytoolsData:CardData[] = [
-  {
-    id: 1,
-    title: "Password Generator",
-    description: "Generate secure passwords with strength analysis.",
-    link: "/password-generator",
-    icon: "/icons/pass.svg", 
-  },
+  // {
+  //   id: 1,
+  //   title: "Password Generator",
+  //   description: "Generate secure passwords with strength analysis.",
+  //   link: "/password-generator",
+  //   icon: "/icons/pass.svg", 
+  // },
 
-  {
-    id: 2,
-    title: "Phishing site detector",
-    description: "Identify potential phishing websites and stay safe online",
-    link: "/phishing-site-detector",
-    icon: "/icons/warning.svg", 
-  },
+  // {
+  //   id: 2,
+  //   title: "Phishing site detector",
+  //   description: "Identify potential phishing websites and stay safe online",
+  //   link: "/phishing-site-detector",
+  //   icon: "/icons/warning.svg", 
+  // },
 
 
   {
